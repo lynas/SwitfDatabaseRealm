ce6a39fa00094f01e5b51d05501361ba389730cb
sazzad <sazzad.islam@dsinnovators.com>
2017-01-18 16:54:49 +0600

    initial commit

---
+++ DatabaseTestProject/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  DatabaseTestProject
+//
+//  Created by sazzad on 1/18/17.
+//  Copyright © 2017 Dynamic Solution Innovators. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textName: UITextField!
+    
+    @IBOutlet weak var textAge: UITextField!
+
+    @IBOutlet weak var textId: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func btnSave(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnView(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnUpdate(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnDelete(_ sender: UIButton) {
+    }
+    
+}
+
